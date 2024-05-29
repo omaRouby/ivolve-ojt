c28e01d7f7f692ea4bfd21dd9924141b44d65383
Omar Ahmed Rouby <164004472+omaRouby@users.noreply.github.com>
2024-05-29 14:48:28 +0300

    Create main.tf

---
+++ terraform/lab-25/modules/ec2/main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  tags = {
+    Name = var.instance_name
+  }
+
+  key_name = var.key_name
+}
+
