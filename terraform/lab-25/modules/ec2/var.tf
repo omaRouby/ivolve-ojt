efad5775eab2d6098c488deea59591a0d19e3102
Omar Ahmed Rouby <164004472+omaRouby@users.noreply.github.com>
2024-05-29 14:49:16 +0300

    Create var.tf

---
+++ terraform/lab-25/modules/ec2/var.tf
@@ -0,0 +1,25 @@
+variable "ami_id" {
+  description = "The AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The type of the EC2 instance"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "The subnet ID for the EC2 instance"
+  type        = string
+}
+
+variable "key_name" {
+  description = "The name of the key pair to use for the EC2 instance"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "The name of the EC2 instance"
+  type        = string
+}
+
