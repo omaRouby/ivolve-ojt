78fc79fc864a4ec4d84bf393236fb4612ce129e5
Omar Ahmed Rouby <164004472+omaRouby@users.noreply.github.com>
2024-05-29 14:45:48 +0300

    Create var.tf

---
+++ terraform/lab-25/modules/networking/var.tf
@@ -0,0 +1,25 @@
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "The name of the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "The CIDR block for the public subnet"
+  type        = string
+}
+
+variable "private_subnet_cidrs" {
+  description = "The CIDR blocks for the private subnets"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
