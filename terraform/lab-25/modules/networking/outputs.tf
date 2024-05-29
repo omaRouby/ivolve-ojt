9b040574eb1a9ffef916f49939dbb6f13d47d325
Omar Ahmed Rouby <164004472+omaRouby@users.noreply.github.com>
2024-05-29 14:45:18 +0300

    Create outputs.tf

---
+++ terraform/lab-25/modules/networking/outputs.tf
@@ -0,0 +1,16 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "igw_id" {
+  value = aws_internet_gateway.igw.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public.id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
